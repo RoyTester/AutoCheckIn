ddf1406611fbe65110e7ca1051d957c809233e4e
zhonlaoda <zhonlaoda@github.com>
2018-10-18 16:36:15 +0800

    sspanel站点自动签到

--- AutoCheckIn.py
+++ AutoCheckIn.py
@@ -1,7 +1,6 @@
 # coding:utf-8
 
 import requests
-from sys import argv
 import warnings
 
 warnings.filterwarnings('ignore')
@@ -9,10 +8,12 @@ s = requests.Session()
 header = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
 }
+file = 'AutoCheckIn.txt'
 
 
 def url_check(url, data):
     r = s.post(url, headers=header, data=data, verify=False)
+    # 某些站点域名采用user.前缀
     if r.status_code != requests.codes.ok:
         url = url[:7] + 'user.' + url[7:]
         url_check(url, data)
@@ -32,13 +33,12 @@ def checkin():
         url_check(url, data)
         checkin_url = '{}/user/checkin'.format(url.rstrip('/auth/login'))
         url_check(checkin_url, data='')
-    except Exception as e:
-        print('检查网络及文本格式', e)
+    except IOError as e:
+        print('检查同名文本是否存在', e)
 
 
 if __name__ == '__main__':
     try:
-        script, file = argv
-    except:
-        print('输入格式应为脚本名+空格+同目录下文本名')
-    checkin()
+        checkin()
+    except ConnectionError as e:
+        print('确保站点可访问', e)
--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # AutoCheckIn
 useage:
 ```console
-python AutoCheckIn.py url.txt
+python AutoCheckIn.py
 ```
+# Spider_Movie
+```console
+python Spider_Movie.py
+```
+or \
+Run dist/Spider_Movie.exe
