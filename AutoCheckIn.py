72cacaeea456f04014a46a5219723f2514fa75f8
zhonlaoda <zhonlaoda@github.com>
2018-12-11 20:48:23 +0800

    sspanel站点签到

--- AutoCheckIn.py
+++ AutoCheckIn.py
@@ -1,44 +1,56 @@
 # coding:utf-8
-
+import re
 import requests
 import warnings
 
 warnings.filterwarnings('ignore')
-s = requests.Session()
-header = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
-}
-file = 'AutoCheckIn.txt'
 
 
-def url_check(url, data):
-    r = s.post(url, headers=header, data=data, verify=False)
-    # 某些站点域名采用user.前缀
-    if r.status_code != requests.codes.ok:
-        url = url[:7] + 'user.' + url[7:]
-        url_check(url, data)
-    else:
-        print(r.json()['msg'])
+class CheckIn:
+    session = requests.Session()
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+        'Chrome/63.0.3239.132 Safari/537.36'
+    }
 
-
-def checkin():
-    try:
-        with open(str(file), 'r') as f:
-            content = f.readlines()
-        data = {
-            'email': content[1].strip('\n'),
-            'passwd': content[2].strip('\n'),
+    def __init__(self, email, password, url):
+        self.postDict = {
+            'email': email,
+            'passwd': password
         }
-        url = 'http://{}/auth/login'.format(content[0].strip('\n'))
-        url_check(url, data)
-        checkin_url = '{}/user/checkin'.format(url.rstrip('/auth/login'))
-        url_check(checkin_url, data='')
-    except IOError as e:
-        print('检查同名文本是否存在', e)
+        self.login_url = '{}/auth/login'.format(url)
+        self.checkin_url = '{}/user/checkin'.format(url)
+        self.user_url = '{}/user'.format(url)
+
+    def login(self):
+        r = self.session.post(self.login_url, headers=self.headers, data=self.postDict, verify=False)
+        data = r.json()
+        return data['msg']
+
+    def checkin(self):
+        """签到
+        :param self:
+        :return:
+        """
+        r = self.session.post(self.checkin_url, headers=self.headers, verify=False)
+        data = r.json()
+        return data['msg']
+
+    def info(self):
+        """显示剩余流量
+        :return:
+        """
+        r = self.session.get(self.user_url, headers=self.headers, verify=False)
+        tmp = re.findall(r'legendText:"(.*)",', r.text)
+        return tmp
+
+    def crack(self):
+        print(self.login(), self.checkin(), self.info())
 
 
 if __name__ == '__main__':
     try:
-        checkin()
+        c = CheckIn(email='email', password='password', url='url')
+        c.crack()
     except ConnectionError as e:
         print('确保站点可访问', e)
